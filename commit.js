f88cfc9522dff8d69a3f8c014c3ade83687aba2b
boss683 <adamhuseinhussein27@gmail.com>
2025-10-16 10:38:18 +0300

    this work about array

---
+++ commit.js
@@ -0,0 +1,469 @@
+
+
+
+
+
+//  switch(new Date().getDay())
+//  {
+//     case 0:
+//         day = "Sunday";
+//          break;
+//     case 1:
+//         break;
+//     case 2:
+//         day = "Tuesday";
+//         break;
+//     case 3:
+//          day = "Wednesday";
+//          break;
+//      case 4:
+//         day = "Thursday";
+//         break;
+//     case 5:
+//         day = "Friday";
+//          break;
+//     case 6:
+//         day = "Saturday";
+//      }
+//  console.log(`day is the ${day}`);
+
+
+
+//  let time = new Date().getHours();
+// let greeting;
+//  if (time < 10) {
+//    greeting = "Good morning";
+//  } 
+//  else if (time < 20) {
+//   greeting = "Good day";
+//  }
+//   else {
+//    greeting = "Good evening";
+// }
+// console.log(greeting);
+ 
+// // nested if statements 
+// // let age = 25;
+// // let game = "Abdalsalam";
+// // if (age >= 18) 
+// //     console.log("You are an adult.")
+// //     if (game === "Abdalsalam") {
+// //         console.log("Welcome, Abdalsalam!");
+// //     }
+// //     else{
+// //         console.log("Welcome, guest!");
+// //     }
+
+
+
+//     // function
+//     function myFunction(){
+//         console.log("maham camp")
+
+//     }
+//     myFunction();
+
+
+//     let tell ="hussein"
+//     function printname (){
+
+//     console. log (tell)
+
+//     }
+//     // parent
+//     function calcutesum(num1,num2){
+//         return num1 + num2;
+//     }
+//     console.log(calcutesum(1,9 ))
+
+
+//     // lock 
+//     function calcutesum (){
+// let a = 4;
+// return a * a ;
+//     }
+//     console.log(calcutesum());
+
+
+
+// // if you sccsses
+
+//   let graded = "100";
+//  score= 95;
+
+// switch (grade) {
+//   case "A":
+//     if (score > 90) {               // condition inside switch case
+//       console.log("Excellent! You got a high A.");
+//     } else {
+//       console.log("Good job! You got a low A.");
+//     }
+//     break;
+
+//   case "B":
+//     if (score > 80) {
+//       console.log("Nice! You got a strong B.");
+//     }
+    
+//      else {
+//       console.log("Keep improving! You got a low B.");
+//     }
+//     break;
+
+//   default:
+//     console.log("Grade not recognized.");
+// }
+// // goalp 
+// let adam = "alight"
+// function display(){
+//     console.log(adam)
+
+// }
+// display();
+
+
+// // lock scope
+// function dispiay1(){
+
+// let rey = 13
+// console.log(rey)
+
+// }
+// dispiay1();
+ 
+// // return
+
+
+// function dispiay2(a){
+//     return a * a;
+//     console.log ( display(3))
+//      console.log( "futuer nrear time in past")
+
+// } 
+
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// }
+
+
+
+// multiplication
+//  let num = 12;
+// console.log("1 x "+num +"=", 1*num);
+// console.log("2 x "+num +"=", 2*num);
+// console.log("3 x "+num +"=", 3*num);
+// console.log("4 x "+num +"=", 4*num);
+// console.log("5 x "+num +"=", 5*num);
+// console.log("6 x "+num +"=", 6*num);
+//  console.log("7 x "+num +"=", 7*num);
+//  console.log("8 x "+num +"=", 8*num);
+//  console.log("9 x "+num +"=", 9*num);
+//  console.log("10 x "+num +"=", 10*num);
+//  console.log("11 x "+num +"=", 11*num);
+//  console.log("12 x "+num +"=", 12*num);
+    
+
+
+//  loop
+
+// for (let n=1;n <10; n++){
+//   console.log("number",n);
+// }
+
+
+// // dicrece
+
+//  for (let m=100;m >0; m--){
+//   console.log("for (let m=100;m >0; m--) ",m*2);
+
+// }
+// for (let hd=3;hd<=12; hd ++){
+//   console.log("6x"+hd+"=",hd*1)
+// }
+
+
+
+
+// //  I  will back  this worke 
+// for ( let hussein =1; hussein <= 10; hussein ++){
+//    console.log(hussein);
+
+//   if(hussein===6){
+
+//     console.log("thes is life in the mahama is this good",hussein);
+//     break;
+//   }
+// }
+
+// for ( let hussein =1; hussein <= 10; hussein ++){
+//    console.log(hussein);
+
+//   if(hussein===6){
+
+//     console.log("thes is life in the mahama is this good",hussein);
+//     break;
+//   }
+
+//  }
+
+// for ( let hussein =1; hussein <= 10; hussein ++){
+//    console.log(hussein);
+
+//   if(hussein===6){
+
+//     console.log("thes is life in the mahama is this good",hussein);
+//     break;
+//   }
+//  }
+
+
+
+
+// //  loop while
+// Do while 
+//    let i=10
+//    while(i<10){
+//     console.log(i);
+//     i++;
+
+//    }
+
+//  let harmane =10;
+// do {
+// console.log(harmane)
+// harmane ++;
+
+// } while(harmane<10)
+
+
+//   let sum = 0;
+// let 
+// for (){
+
+
+// }
+
+// let sum=0;
+// for (let i = 1; i < 100;i++) {
+//   if(i%2 === 0) { 
+//    console.log(sum+=i);       
+//   }
+// }
+
+// let sum=0;
+// for (let x = 1; x < 100;x++) {
+//   if(x% 3=== 0) { 
+//    console.log(sum+=x);       
+//   }
+// }
+
+// let num=2;
+// let a = prompt (5)
+
+//  console.log("2 x 2 =",2*num);
+//  console.log("3 x 2 =",3*num);
+//  console.log("4 x 2 =",4*num);
+//  console.log("5 x 2 =",5*num);
+// console.log("6 x 2 =",6*num);
+//  console.log("7 x 2 =",7*num);
+// console.log("8 x 2 =",8*num);
+// console.log("9 x 2 =",9*num);
+// console.log("10 x 2 =",10*num);
+
+// let fgima= 20;
+// for( fgima; 20;fgima ++){
+//    console.log("figma",fgima);
+
+// }
+// // 
+//
+// do {
+//   let life1 = +prompt("Enter first number:");
+//   let operator = + prompt("Enter operator (+, -, *, /, %):");
+//   let life2 = +prompt(" Enter second number:");
+//   let result;
+
+//   switch (operator) {
+//     case "+": result = life1 + life2; break;
+//     case "-": result = life1 - life2; break;
+//     case "*": result = life1 * life2; break;
+//     case "/": result = life1 / life2; break;
+//     case "%": result = life1 % life2; break;
+//     default: console.log("Invalid operator");
+//   }
+
+//   console.log(`Result: ${result}`);
+//   console.log(result % 2 === 0? "Even" : "Odd"); 
+
+//  }
+//  while (confirm("Do another calculation?"));
+// my worke 
+// do{  
+// let www1 = + prompt ("enter your number first ");
+// let opertor =+prompt ("Enter operater (+,-,*,/,%):");
+// let www2 =+ prompt("Enter second number:")
+// let reslut;
+// switch(operater){
+//   case "+": result = www1 + www2 ;break;
+//   case "-":result = www1 - www2 ;break;
+//   case "*":result =www1 * www2 ;break ;
+//   case "%": result =www1 % www2 ;break ;
+//   case "/": result = www1 / www2 ;break ;
+//   default: console.log("invalid oprater")
+// }
+// console.log("result ${result}")
+// console.log( result %2 === 0 ? "Even":"odd");
+// }
+// while (confirm("do anther calulation"))
+
+
+
+
+  
+  // 
+// let a=1
+// let b =15
+// for(let  =1;a<b;b++){
+//   console.log()
+// }
+
+// let n =20 
+// for(lte i=0;i<n;i++);     
+
+
+
+
+
+//  area premiter 
+// let l=5;
+// let w=4;
+// let a;
+// let p;
+// a=(w*l);
+// console.log("the area  ",a)
+// p=(l+w)
+// console.log("the peruter",p)
+
+
+
+
+// let weight=prompt('Enter your weight');
+// let hight=prompt('Enter your hight');
+
+// let BMI = weight /(hight*hight);
+
+// if (BMI<=18.4){
+//     console.log(BMI +''+"underWeight");
+// }
+// else if (BMI>=25 && BMI<= 24.9){
+//     console.log(BMI +''+ +''+"normal");
+// }
+
+
+// else if (BMI>=25 && BMI<= 39.9){
+//     console.log(BMI +''+ +''+"OverWeight");
+// }
+
+
+
+// else if (BMI>=40){
+//     console.log(BMI +''+ "Over lod");
+// }
+
+
+
+
+// let hight= prompt("enter your mass");
+// let hight1= prompt("enter your hight ");
+// let MBI= hight/(hight*hight)
+// if (BMI<=18.4){
+//   console.log(BMI + ''+ "normal")
+// }
+// else if( BMI >=25 && BMI <= 24.9){
+//   console.log(BMI+''++ '+')
+// }
+
+
+
+
+
+// let t;
+// result;
+// prompt("enter any number ")
+// for(t++)
+
+// console.log(t)
+
+
+
+
+
+  // let sum = 0;
+  // let count = 0;
+
+  // for (let i = +10; i <= 90; i++) {
+  //   if (i % 4 === 0) {
+  //     sum += i;
+  //     count++;
+  //   }
+  // }
+
+  // let average = sum / count;
+  // console.log("The average of even numbers between 10 and 70 is:", average);
+
+
+// let sum = 0;
+// let count=0;
+// for ( let r =+10; r <=80;r++){
+//   if (r % 4===0){
+//     sum += r;
+//     count++;
+//   }
+// }
+//  let average = sum / count ;
+//   console.log ("the average of even numbers between 10 and 90 is :",average)
+
+
+// porgrameng helpe the user seclcter
+
+
+  let category = prompt("electronics, clothing, or food?");
+
+switch(category) {
+    case "electronics":
+        let elec = prompt("computer, smartphone, or headphones?");
+        switch(elec) {
+            case "computer": console.log("$2000"); break;
+            case "smartphone": console.log("$800"); break;
+            case "headphones": console.log("$150"); break;
+        }
+        break;
+        
+    case "clothing":
+        let cloth = prompt("tshirt, jeans, or jacket?");
+        switch(cloth) {
+            case "tshirt": console.log("$25"); break;
+            case "jeans": console.log("$60"); break;
+            case "jacket": console.log("$120"); break;
+        }
+        break;
+        
+    case "food":
+        let food = prompt("pizza, burger, or salad?");
+        switch(food) {
+            case "pizza": console.log("$15"); break;
+            case "burger": console.log("$8"); break;
+            case "salad": console.log("$12"); break;
+        }
+        break;
+}
+
+
+  
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,469 +1,236 @@
+// In JavaScript, an array is a special type of object used to store and manage an ordered collection of values.These values, also known as elements, can be of any data type, including numbers, strings, booleans, objects, or even other arrays.
 
 
 
+// Shopping Cart Items:
+let cartItems = ["Milk", "Bread", "Eggs", "Butter"];
+console.log(cartItems);
+// Daily Temperatures for a Week:
+let temperatures = [25, 28, 27, 26, 30, 29, 31];
+console.log(temperatures)
+//Contacts in a Phone:
+let contacts = ["Alice", "Bob", "Charlie", "Diana"];
+console.log(contacts)
+
+// Q2. How do you create an array?
 
 
-//  switch(new Date().getDay())
-//  {
-//     case 0:
-//         day = "Sunday";
-//          break;
-//     case 1:
-//         break;
-//     case 2:
-//         day = "Tuesday";
-//         break;
-//     case 3:
-//          day = "Wednesday";
-//          break;
-//      case 4:
-//         day = "Thursday";
-//         break;
-//     case 5:
-//         day = "Friday";
-//          break;
-//     case 6:
-//         day = "Saturday";
-//      }
-//  console.log(`day is the ${day}`);
+// Using square brackets (most common way):
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits)
+// This creates an array with three elements: "Apple", "Banana", and "Mango".
+
+// 
+
+// 2. Using the new Array() constructor:
+let names = new Array("hussein", "omer", "Majed");
+console.log(names)
 
 
+//  This does the same thing but is less common — most developers
+// prefer the first metho
 
-//  let time = new Date().getHours();
-// let greeting;
-//  if (time < 10) {
-//    greeting = "Good morning";
-//  } 
-//  else if (time < 20) {
-//   greeting = "Good day";
-//  }
-//   else {
-//    greeting = "Good evening";
-// }
-// console.log(greeting);
+
+// You can also create an empty array and add items later:
+let food = [];
+food.push("rize");
+food.push("fish");
+console.log(food)
+
+// The types of data that can be stored in an array depend 
+// on the programming language and its specific implementation of arrays.
+
+// 1 - Statically-typed languages (e.g., Java, C, C#)
+// 2- Dynamically-typed languages (e.g., JavaScript, Python, PHP)
+
+// 1- Numbers: Integers and floating-point numbers can coexist in the same array.
+// 2- Strings: Sequences of characters can be mixed with other types.
+// 3- Booleans: true or false values.
+// 4 -Objects: Custom or built-in object types, such as dates or functions.
+// 5 - Nested arrays: You can create an array that contains other arrays.
+//6 - Other values: Many dynamic languages also allow null and undefined values in an array. 
+
+
+// Create one array that mixes numbers, strings, and booleans.
+let alight = [25, "Hello", true, 3.14, "JavaScript", false];
+console.log(alight)
+
+
+// Q4. What is the difference between an array and a single variable?
+
+// A single variable stores one value, while an array stores multiple values 
+// of the same data type in a single, ordered collection. An array allows you
+//  to manage and perform operations on large sets of related data more efficiently
+// //   than with individual variables
+
+// Explain in your own words and give one code example to support your answer.
+let mixedArray = [25, "Hello", true];
+console.log(mixedArray);
+
+[25, "Hello", true]
+     
+// Accessing and Modifying Arrays
+
+// Arrays allow you to store a collection of data under a single variable name 
+// and access or modify it by referring to its position, or index. While the basic 
+// bracket [] notation is universal for both accessing and modifying, most programming
+//  languages provide a host of other methods to manipulate arrays in 
+//  more advanced ways, such as adding or removing elements. 
  
-// // nested if statements 
-// // let age = 25;
-// // let game = "Abdalsalam";
-// // if (age >= 18) 
-// //     console.log("You are an adult.")
-// //     if (game === "Abdalsalam") {
-// //         console.log("Welcome, Abdalsalam!");
-// //     }
-// //     else{
-// //         console.log("Welcome, guest!");
-// //     }
 
 
+//             How do you access specific items from an array?
 
-//     // function
-//     function myFunction(){
-//         console.log("maham camp")
+// The first element is index 0, the second is 1, the third is 2, and so on.
 
-//     }
-//     myFunction();
+let book = ["pen", "pencel", "bak"];
+console.log(book[0]); 
+console.log(book[1]); 
+console.log(book[2]); 
 
-
-//     let tell ="hussein"
-//     function printname (){
 
-//     console. log (tell)
+//     Write a program that prints the first and last elements of your array.
 
-//     }
-//     // parent
-//     function calcutesum(num1,num2){
-//         return num1 + num2;
-//     }
-//     console.log(calcutesum(1,9 ))
+let mama = ["sister", "me", "brather", "sister1",];
 
 
-//     // lock 
-//     function calcutesum (){
-// let a = 4;
-// return a * a ;
-//     }
-//     console.log(calcutesum());
+console.log("First element:", mama[0]);
 
+console.log("Last element:", mama[mama.length - 1]);
 
 
-// // if you sccsses
+// How do you change a value in an array
 
-//   let graded = "100";
-//  score= 95;
+let change = ["css", "html", "javascript"];
 
-// switch (grade) {
-//   case "A":
-//     if (score > 90) {               // condition inside switch case
-//       console.log("Excellent! You got a high A.");
-//     } else {
-//       console.log("Good job! You got a low A.");
-//     }
-//     break;
+// Change "html" to "ghthup"
+change[1] = "ghthup";
 
-//   case "B":
-//     if (score > 80) {
-//       console.log("Nice! You got a strong B.");
-//     }
-    
-//      else {
-//       console.log("Keep improving! You got a low B.");
-//     }
-//     break;
+console.log(change);
 
-//   default:
-//     console.log("Grade not recognized.");
-// }
-// // goalp 
-// let adam = "alight"
-// function display(){
-//     console.log(adam)
+// Change one of the elements in your array and print the new array.
 
-// }
-// display();
+let colors = ["Red", "Blue", "Green"];
 
+// Change "Blue" to "Yellow"
+colors[1] = "Yellow";
 
-// // lock scope
-// function dispiay1(){
+// Print the new array
+console.log(colors);
 
-// let rey = 13
-// console.log(rey)
 
-// }
-// dispiay1();
- 
-// // return
 
+// length in JavaScript
 
-// function dispiay2(a){
-//     return a * a;
-//     console.log ( display(3))
-//      console.log( "futuer nrear time in past")
+// The .length property of an array tells you how many elements are in the array.
+// It counts all items, starting from the first to the last
+let paste = ["i", "me", "you", "ther"];
 
-// } 
+// Use .length to find out how many items are in the array
+console.log("Number of items in the array:", paste.length);
 
-// let age = 18;
 
-// if (age >= 18) {
-//   console.log("You are an adult.");
-// }
 
+// 8. How can you add a new element to the end of an array without using .push()? Use array index assignment instead.
+let fruits1 = ["Apple", "Banana", "Mango"];
 
+// Add a new element to the end
+fruits1[fruits1.length] = "Orange";
 
-// multiplication
-//  let num = 12;
-// console.log("1 x "+num +"=", 1*num);
-// console.log("2 x "+num +"=", 2*num);
-// console.log("3 x "+num +"=", 3*num);
-// console.log("4 x "+num +"=", 4*num);
-// console.log("5 x "+num +"=", 5*num);
-// console.log("6 x "+num +"=", 6*num);
-//  console.log("7 x "+num +"=", 7*num);
-//  console.log("8 x "+num +"=", 8*num);
-//  console.log("9 x "+num +"=", 9*num);
-//  console.log("10 x "+num +"=", 10*num);
-//  console.log("11 x "+num +"=", 11*num);
-//  console.log("12 x "+num +"=", 12*num);
-    
+// Print the updated array
+console.log(fruits);
 
 
-//  loop
 
-// for (let n=1;n <10; n++){
-//   console.log("number",n);
-// }
+// Here’s a program that prints all elements of an array using three different types of loops in JavaScript
+let fruits5 = ["Apple", "Banana", "Mango", "Orange"];
 
+console.log("Using for loop:");
+for (let i = 0; i < fruits5.length; i++) {
+  console.log(fruits5[i]);
+}
 
-// // dicrece
+console.log("\nUsing for...of loop:");
+for (let fruit of fruits5) {
+  console.log(fruit);
+}
 
-//  for (let m=100;m >0; m--){
-//   console.log("for (let m=100;m >0; m--) ",m*2);
-
-// }
-// for (let hd=3;hd<=12; hd ++){
-//   console.log("6x"+hd+"=",hd*1)
-// }
-
-
-
-
-// //  I  will back  this worke 
-// for ( let hussein =1; hussein <= 10; hussein ++){
-//    console.log(hussein);
-
-//   if(hussein===6){
-
-//     console.log("thes is life in the mahama is this good",hussein);
-//     break;
-//   }
-// }
-
-// for ( let hussein =1; hussein <= 10; hussein ++){
-//    console.log(hussein);
-
-//   if(hussein===6){
-
-//     console.log("thes is life in the mahama is this good",hussein);
-//     break;
-//   }
-
-//  }
-
-// for ( let hussein =1; hussein <= 10; hussein ++){
-//    console.log(hussein);
-
-//   if(hussein===6){
-
-//     console.log("thes is life in the mahama is this good",hussein);
-//     break;
-//   }
-//  }
-
-
-
-
-// //  loop while
-// Do while 
-//    let i=10
-//    while(i<10){
-//     console.log(i);
-//     i++;
-
-//    }
-
-//  let harmane =10;
-// do {
-// console.log(harmane)
-// harmane ++;
-
-// } while(harmane<10)
-
-
-//   let sum = 0;
-// let 
-// for (){
-
-
-// }
-
-// let sum=0;
-// for (let i = 1; i < 100;i++) {
-//   if(i%2 === 0) { 
-//    console.log(sum+=i);       
-//   }
-// }
-
-// let sum=0;
-// for (let x = 1; x < 100;x++) {
-//   if(x% 3=== 0) { 
-//    console.log(sum+=x);       
-//   }
-// }
-
-// let num=2;
-// let a = prompt (5)
-
-//  console.log("2 x 2 =",2*num);
-//  console.log("3 x 2 =",3*num);
-//  console.log("4 x 2 =",4*num);
-//  console.log("5 x 2 =",5*num);
-// console.log("6 x 2 =",6*num);
-//  console.log("7 x 2 =",7*num);
-// console.log("8 x 2 =",8*num);
-// console.log("9 x 2 =",9*num);
-// console.log("10 x 2 =",10*num);
-
-// let fgima= 20;
-// for( fgima; 20;fgima ++){
-//    console.log("figma",fgima);
-
-// }
-// // 
-//
-// do {
-//   let life1 = +prompt("Enter first number:");
-//   let operator = + prompt("Enter operator (+, -, *, /, %):");
-//   let life2 = +prompt(" Enter second number:");
-//   let result;
-
-//   switch (operator) {
-//     case "+": result = life1 + life2; break;
-//     case "-": result = life1 - life2; break;
-//     case "*": result = life1 * life2; break;
-//     case "/": result = life1 / life2; break;
-//     case "%": result = life1 % life2; break;
-//     default: console.log("Invalid operator");
-//   }
-
-//   console.log(`Result: ${result}`);
-//   console.log(result % 2 === 0? "Even" : "Odd"); 
-
-//  }
-//  while (confirm("Do another calculation?"));
-// my worke 
-// do{  
-// let www1 = + prompt ("enter your number first ");
-// let opertor =+prompt ("Enter operater (+,-,*,/,%):");
-// let www2 =+ prompt("Enter second number:")
-// let reslut;
-// switch(operater){
-//   case "+": result = www1 + www2 ;break;
-//   case "-":result = www1 - www2 ;break;
-//   case "*":result =www1 * www2 ;break ;
-//   case "%": result =www1 % www2 ;break ;
-//   case "/": result = www1 / www2 ;break ;
-//   default: console.log("invalid oprater")
-// }
-// console.log("result ${result}")
-// console.log( result %2 === 0 ? "Even":"odd");
-// }
-// while (confirm("do anther calulation"))
-
-
-
-
-  
-  // 
-// let a=1
-// let b =15
-// for(let  =1;a<b;b++){
-//   console.log()
-// }
-
-// let n =20 
-// for(lte i=0;i<n;i++);     
-
-
-
-
-
-//  area premiter 
-// let l=5;
-// let w=4;
-// let a;
-// let p;
-// a=(w*l);
-// console.log("the area  ",a)
-// p=(l+w)
-// console.log("the peruter",p)
-
-
-
-
-// let weight=prompt('Enter your weight');
-// let hight=prompt('Enter your hight');
-
-// let BMI = weight /(hight*hight);
-
-// if (BMI<=18.4){
-//     console.log(BMI +''+"underWeight");
-// }
-// else if (BMI>=25 && BMI<= 24.9){
-//     console.log(BMI +''+ +''+"normal");
-// }
-
-
-// else if (BMI>=25 && BMI<= 39.9){
-//     console.log(BMI +''+ +''+"OverWeight");
-// }
-
-
-
-// else if (BMI>=40){
-//     console.log(BMI +''+ "Over lod");
-// }
-
-
-
-
-// let hight= prompt("enter your mass");
-// let hight1= prompt("enter your hight ");
-// let MBI= hight/(hight*hight)
-// if (BMI<=18.4){
-//   console.log(BMI + ''+ "normal")
-// }
-// else if( BMI >=25 && BMI <= 24.9){
-//   console.log(BMI+''++ '+')
-// }
-
-
-
-
-
-// let t;
-// result;
-// prompt("enter any number ")
-// for(t++)
-
-// console.log(t)
-
-
-
-
-
-  // let sum = 0;
-  // let count = 0;
-
-  // for (let i = +10; i <= 90; i++) {
-  //   if (i % 4 === 0) {
-  //     sum += i;
-  //     count++;
-  //   }
-  // }
-
-  // let average = sum / count;
-  // console.log("The average of even numbers between 10 and 70 is:", average);
-
-
-// let sum = 0;
-// let count=0;
-// for ( let r =+10; r <=80;r++){
-//   if (r % 4===0){
-//     sum += r;
-//     count++;
-//   }
-// }
-//  let average = sum / count ;
-//   console.log ("the average of even numbers between 10 and 90 is :",average)
-
-
-// porgrameng helpe the user seclcter
-
-
-  let category = prompt("electronics, clothing, or food?");
-
-switch(category) {
-    case "electronics":
-        let elec = prompt("computer, smartphone, or headphones?");
-        switch(elec) {
-            case "computer": console.log("$2000"); break;
-            case "smartphone": console.log("$800"); break;
-            case "headphones": console.log("$150"); break;
-        }
-        break;
-        
-    case "clothing":
-        let cloth = prompt("tshirt, jeans, or jacket?");
-        switch(cloth) {
-            case "tshirt": console.log("$25"); break;
-            case "jeans": console.log("$60"); break;
-            case "jacket": console.log("$120"); break;
-        }
-        break;
-        
-    case "food":
-        let food = prompt("pizza, burger, or salad?");
-        switch(food) {
-            case "pizza": console.log("$15"); break;
-            case "burger": console.log("$8"); break;
-            case "salad": console.log("$12"); break;
-        }
-        break;
+console.log("\nUsing while loop:");
+let i = 0;
+while (i < fruits5.length) {
+  console.log(fruits5 [i]);
+  i++;
 }
 
 
-  
+// Q10. Write a program that prints all numbers in an array multiplied by 2 using a loop.
+let numbers = [1, 2, 3, 4, 5];
+
+console.log("Numbers multiplied by 2:");
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] * 2);
+}
+
+// Q11. Create an array of names and print each name in uppercase using a loop.
+
+let names12 = ["Alice", "Bob", "Charlie", "Diana"];
+
+console.log("Names in uppercase:");
+for (let i = 0; i < names12.length; i++) {
+  console.log(names12[i].toUpperCase());
+}
 
 
 
 
+// Here’s a JavaScript program that counts the number of items in an array without using .length, using a for loop:
+let orange= ["Apple", "Banana", "Mango", "Orange"];
+let counts = 0;
 
+for (let item of orange) {
+  counts++; // Increment count for each item
+}
+
+let fruitses = ["Apple", "Banana", "Mango", "Orange"];
+let count = 0;
+
+for (let item of fruits) {
+  count++; // Increment count for each item
+}
+
+console.log("Number of items in the array:", count);
+
+
+
+let  num = [15, 42, 7, 28, 93, 3];
+
+let larges = num[0];
+let smalles = num[0];
+
+for (let i = 1; i < num.length; i++) {
+  if (num[i] > largest) {
+    larges = num[i]; // Update largest if current number is bigger
+  }
+  if (num[i] < smallest) {
+    smallest = num[i]; // Update smallest if current number is smaller
+  }
+}
+
+console.log("Largest number:", larges);
+console.log("Smallest number:", smallest);
+num = [15, 42, 7, 28, 93, 3];
+
+let largest = num[0];
+let smallest = num[0];
+
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > largest) {
+    largest = numbers[i]; // Update largest if current number is bigger
+  }
+  if (numbers[i] < smallest) {
+    smallest = numbers[i]; // Update smallest if current number is smaller
+  }
+}
+
+console.log("Largest number:", largest);
+console.log("Smallest number:", smallest);
